bb80a4bafee7c411648d4461ed442e486f6ca100
Leon-byte-pixel <124020800+Leon-byte-pixel@users.noreply.github.com>
2025-07-23 22:40:07 +0900

    Add files via upload

---
+++ uag-js-219.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"0bb1c552","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-0bb1c552' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"0bb1c552","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-0bb1c552' );
+});
+ });
